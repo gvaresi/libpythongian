d11cf3318dc1da741252a2784ad75e5d69c6d2d7
gvaresi <varesi@bol.com.br>
2020-10-05 19:31:27 -0300

    adicionado busca site api

--- libpythongian/github_api.py
+++ libpythongian/github_api.py
@@ -9,9 +9,14 @@ def buscar_avatar(usuario):
     """
 
     url = f'https://api.github.com/users/{usuario}'
+    url2 = f'https://api.github.com/users/{usuario}'
     resp = requests.get(url)
-    return resp.json()['avatar_url']
+    resp2 = requests.get(url2)
+    return (resp.json()['avatar_url'], resp2.json()['html_url'])
+    # return resp2.json()['html_url']
+
 
 
 if __name__ == '__main__':
-    print(buscar_avatar('gvaresi'))
+    # print(buscar_avatar('gvaresi'))
+    print(buscar_avatar(input('Digite um usuario github para exibir seu avatar: ')))
